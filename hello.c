400a2df9138a94e2b4c5b7591671e1bc2b119478
Haider00ali <130845787+Haider00ali@users.noreply.github.com>
2023-04-17 08:22:19 +0000

    ddd, Apr 4/17/2023, YYYY, 8:22 A +0

--- hello.c
+++ hello.c
@@ -4,15 +4,12 @@
 int main(void)
 {
 
-  // Step ( 1 ) : Print Hello, World
-
-printf("Hello, World\n");
 
 string name = get_string("What's your name   ");
 
 printf("Hello, %s\n",name);
 
 
-  //Step ( 2 ) : Print Hello, Your Name
+
 
 }
